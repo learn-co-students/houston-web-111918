1366367bb036f4761b08ee48f1599ad98abe2125
Michael Cheng <cheng.c.mike@gmail.com>
2018-12-14 10:51:16 -0600

    intro rails

---
+++ 16-intro-rails/cars-app/db/seeds.rb
@@ -0,0 +1,4 @@
+Car.create(make: 'chevy', model: 'truck', wheels: 5)
+Car.create(make: 'dodge', model: 'challenger', wheels: 3)
+Car.create(make: 'toyota', model: 'camry', wheels: 17000)
+Car.create(make: 'bus', model: 'big', wheels: 1)
